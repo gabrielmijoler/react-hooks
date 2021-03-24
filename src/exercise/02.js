489ae1176c25cf8071123e1233af98bfb4110a44
gabrielmijoler <gabrielmijoler@hotmail.com>
2021-03-24 13:57:12 +0000

    exercicio4

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,47 +3,49 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') || initialName
+function Greeting({ initialName = '' }) {
+    // 🐨 initialize the state to the value from localStorage
+    // 💰 window.localStorage.getItem('name') || initialName
 
-  // usestate, quando recebe um valor estatico vai sempre inicializar
-  //o componente em toda atualizacao do componente que houver
-  // para evitar isso, em vez de passsar o valor estatico, passamos uma função
-  // que sera chamada inicializara a variavel de estado apenas quando necessario
-  //Isso chamado ed LAZY INITIALIZER (inicializador"preguicoso")
+    // usestate, quando recebe um valor estatico vai sempre inicializar
+    //o componente em toda atualizacao do componente que houver
+    // para evitar isso, em vez de passsar o valor estatico, passamos uma função
+    // que sera chamada inicializara a variavel de estado apenas quando necessario
+    //Isso chamado ed LAZY INITIALIZER (inicializador"preguicoso")
 
-  const [name, setName] = React.useState(window.localStorage.getItem('name') || initialName )
+    const [name, setName] = React.useState(window.localStorage.getItem('name') || initialName)
+    const [count,setcount] = React.useState
+    // 🐨 Here's where you'll use `React.useEffect`.
+    // The callback should set the `name` in localStorage.
+    // 💰 window.localStorage.setItem('name', name)
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
 
+    React.useEffect(() => {
+        // atualizando o localstorage como efeito colateral da atualizacao
+        //do componente que disparada pela atualização do estado
+        window.localStorage.setItem('name', name)
+        setcount(count + 1)
+    },[name])// dependencia so chama useeffect quando a variavel do estado name for alterada
 
-  React.useEffect(()=>{
-      // atualizando o localstorage como efeito colateral da atualizacao
-      //do componente que disparada pela atualização do estado
-      window.localStorage.setItem('name', name)
-  },[name]
-  )
+    function handleChange(event) {
+        setName(event.target.value)
+    }
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input value={name} onChange={handleChange} id="name" />
+            </form>
+            {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+            <div>localStorage:{window.localStorage.getitem('name')}</div>
+            <div>Contador: {count}</div>
+        </div>
 
-  function handleChange(event) {
-    setName(event.target.value)
-  }
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    <div>localStorage:{window.localStorage.getitem('name')}</div> 
-    </div>
-  )
+    )
 }
 
 function App() {
-  return <Greeting />
+    return <Greeting />
 }
 
 export default App
--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,53 +3,59 @@
 
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
-  return (
-    <div>
-      <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
-    </div>
-  )
+function Name() {
+    const [name, setname] = react.useState('')
+    return (
+        <div>
+            <label htmlFor="name">Name: </label>
+            <input id="name" value={name} onChange={event => setname(e.target.value)} />
+        </div>
+    )
 }
 
 // 🐨 accept `animal` and `onAnimalChange` props to this component
-function FavoriteAnimal() {
-  // 💣 delete this, it's now managed by the App
-  const [animal, setAnimal] = React.useState('')
-  return (
-    <div>
-      <label htmlFor="animal">Favorite Animal: </label>
-      <input
-        id="animal"
-        value={animal}
-        onChange={event => setAnimal(event.target.value)}
-      />
-    </div>
-  )
+function FavoriteAnimal({ animal, onAnimalchange }) {
+    // 💣 delete this, it's now managed by the App
+    //const [animal, setAnimal] = React.useState('')
+
+    return (
+        <div>
+            <label htmlFor="animal">Favorite Animal: </label>
+            <input
+                id="animal"
+                value={animal}
+                onChange={onAnimalchange}
+            />
+        </div>
+    )
 }
+function Display({ animal }) {
+    return <div>{`Your favorite animal is: ${animal}!`}</div>
+    // 🐨 uncomment this
+    //function Display({name, animal}) {
+    //return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
+    //}
 
-// 🐨 uncomment this
-// function Display({name, animal}) {
-//   return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>
-// }
+    // 💣 remove this component in favor of the new one
+    //function Display({name}) {
+    // return <div>{`Hey ${name}, you are great!`}</div>
+    //}
 
-// 💣 remove this component in favor of the new one
-function Display({name}) {
-  return <div>{`Hey ${name}, you are great!`}</div>
-}
+    function App() {
+        // 🐨 add a useState for the animal
+        //const [name, setName] = React.useState('')
+        const [animal, setAnimal] = react.useState('')
+        return (
+            <form>
+                <Name />
+                
+                {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
+                <FavoriteAnimal animal={animal} onAnimalchange={event => setanimal(event.target.value)} />
+                {/* 🐨 pass the animal prop here */}
+                <Display animal={animal} />
+            </form>
+        )
+    }
 
-function App() {
-  // 🐨 add a useState for the animal
-  const [name, setName] = React.useState('')
-  return (
-    <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
-      {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
-      <FavoriteAnimal />
-      {/* 🐨 pass the animal prop here */}
-      <Display name={name} />
-    </form>
-  )
-}
-
-export default App
+    export default App
+// name={name} onNameChange={event => setName(event.target.value)}
